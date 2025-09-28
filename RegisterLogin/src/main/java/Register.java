c04f4bb5da46f108dab75ab82073cee5262a8e72
yashjha2510 <yashjha2510@gmail.com>
2025-09-28 14:16:10 +0530

    successfully develop the login page

--- RegisterLogin/src/main/java/Register.java
+++ RegisterLogin/src/main/java/Register.java
@@ -1,5 +1,11 @@
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.concurrent.CountDownLatch;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -7,10 +13,65 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/regForm")
-public class Register extends HttpServlet {
+public class Register extends HttpServlet  {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// backend code
+		PrintWriter out = resp.getWriter();
+		
+		String uname = req.getParameter("user_name");
+		String uemail = req.getParameter("user_email");
+		String upass = req.getParameter("user_pass");
+		String ugender = req.getParameter("user_gender");
+		String ucity = req.getParameter("user_city");
+		
+		//jdbc connection
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/yashjhaclgdb", "root", "Betu8959@");
+			
+			PreparedStatement ps = con.prepareStatement("insert into register values (?,?,?,?,?)");
+			ps.setString(1, uname);
+			ps.setString(2, uemail);
+			ps.setString(3, upass);
+			ps.setString(4, ugender);
+			ps.setString(5, ucity);
+			
+			int count = ps.executeUpdate();
+			
+			if (count> 0) {
+				System.out.println(count + " row affected");
+				
+				
+				resp.setContentType("text/html");
+				out.println("<h3> successfully registered </h3>");
+				
+				RequestDispatcher rd = req.getRequestDispatcher("/register.jsp");
+				rd.include(req, resp);
+				out.println("<h3>" + count + " row affected </h3>");
+				
+			}
+			else {
+				System.out.println("can not insert");
+				System.out.println(count + " row affected");
+				
+				resp.setContentType("text/html");
+				out.println("<h3> not registered </h3>");
+				
+				RequestDispatcher rd = req.getRequestDispatcher("/register.jsp");
+				rd.include(req, resp);
+			}
+			
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+			
+			resp.setContentType("text/html");
+			out.println("<h3> Exceptin occured : " + e.getMessage() + "</h3>");
+			
+			RequestDispatcher rd = req.getRequestDispatcher("/register.jsp");
+			rd.include(req, resp);
+		}
 		
 	}
 	
--- RegisterLogin/src/main/webapp/register.jsp
+++ RegisterLogin/src/main/webapp/register.jsp
@@ -7,13 +7,13 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<form action="/RegisterLogin/regForm" method="post">
+	<form action="regForm" method="post">
 		Name : <input type="text" name="user_name"> <br> <br>
-		Email : <input type="text" name="user_email"> <br> <br>
-		Password : <input type="text" name="user_pass"> <br> <br>
-		Gender : <input type="radio" name="user_gender">Male
-				<input type="radio" name="user_gender">Female
-				<input type="radio" name="user_gender">others
+		Email : <input type="email" name="user_email"> <br> <br>
+		Password : <input type="password" name="user_pass"> <br> <br>
+		Gender : <input type="radio" name="user_gender" value="male">Male
+				<input type="radio" name="user_gender" value="female">Female
+				<input type="radio" name="user_gender" value="others">others
 		 <br> <br>
 		City : <select name="user_city">
 				<option>Select City</option>
