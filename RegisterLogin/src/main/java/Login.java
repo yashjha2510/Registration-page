ddc9a4c4ab6c9b666492888dcae626337a6eba1b
yashjha2510 <yashjha2510@gmail.com>
2025-09-28 19:33:43 +0530

    successfully develop the redirect feature to the profile page

---
+++ RegisterLogin/src/main/java/Login.java
@@ -0,0 +1,57 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.security.interfaces.RSAKey;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/loginForm")
+public class Login extends HttpServlet {
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		PrintWriter out = resp.getWriter();
+		resp.setContentType("text/html");
+		
+		String uemail = req.getParameter("user_email");
+		String upass = req.getParameter("user_pass");
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/yashjhaclgdb","root","Betu8959@");
+			
+			PreparedStatement ps = con.prepareStatement("select * from register where email = ? and pass = ?");
+			ps.setString(1, uemail);
+			ps.setString(2, upass);
+			
+			ResultSet rs = ps.executeQuery();
+			
+			if(rs.next()) {
+				System.out.println("successfully login");
+				RequestDispatcher rd = req.getRequestDispatcher("/profile.jsp");
+				rd.include(req, resp);
+			}
+			else {
+				RequestDispatcher rd = req.getRequestDispatcher("/login.jsp");
+				rd.include(req, resp);
+				
+				out.println("<h3> Email id and Password didn't match </h3>");
+			}
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+			RequestDispatcher rd = req.getRequestDispatcher("/login.jsp");
+			rd.include(req, resp);
+			
+			out.println("<h3> Exception occured : " + e.getMessage() + "</h3>");
+		}
+	}
+}
---
+++ RegisterLogin/src/main/webapp/login.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<form action="loginForm" method="post">
+		Email : <input type="email" name="user_email"> <br> <br>
+		Password : <input type="password" name="user_pass"> <br> <br>
+		<input type="submit" value="Login"> <br> <br>
+	</form>
+</body>
+</html>
